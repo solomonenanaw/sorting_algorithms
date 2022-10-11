0736c399cbe7fcac0888accfa1cdfa397fbe3c80
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2022-10-11 20:41:12 +0300

    Update 0-bubble_sort.c
    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,47 @@
 #include "sort.h"
 
 /**
-  * bubble_sort - A function for bubble sorting.
-  * @array: The array to sort.
-  * @size: THe length of the aaray.
-  * Return: Nothing.
-  */
+ * swap_values - swaps 2 values in an array of ints
+ *
+ * @array: the array of ints
+ * @i1: index of first value
+ * @i2: index of 2nd value
+ *
+ * Return: the array with value
+ */
+
+void swap_values(int **array, ssize_t i1, ssize_t i2)
+{
+	int tmp;
+
+	tmp = (*array)[i1];
+	(*array)[i1] = (*array)[i2];
+	(*array)[i2] = tmp;
+}
+
+/**
+ * bubble_sort - sorts an array of integers using bubble sort
+ *
+ * @array: the array of integers
+ * @size: the size of the array
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int aux = 0;
+	size_t n = size, new_n, i;
 
-	if (array == NULL || size == 0)
-		return;
-	for (; i < size - 1; i++)
+	while (n > 1)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		new_n = 0;
+		for (i = 1; i <= n - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i - 1] > array[i])
 			{
-				aux = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = aux;
+				swap_values(&array, i - 1, i);
 				print_array(array, size);
+				new_n = i;
 			}
 		}
+		n = new_n;
 	}
 }
